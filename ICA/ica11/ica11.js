40f3862f9be2bb01ab37cd13e3ac7d0bda37bb7e
chsc9796 <chsc9796@colorado.edu>
2022-11-01 10:41:01 -0600

    ica11

---
+++ ICA/ica11/ica11.js
@@ -0,0 +1,33 @@
+//document.addEventListener('load', getQuote);
+
+const quoteButton = document.querySelector('.new-quote');
+
+quoteButton.addEventListener('click', getQuote);
+
+const endpoint = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
+
+async function getQuote() {
+    //console.log('test click worked');
+    let text = await fetch(endpoint);
+    let response = await text.text();
+
+    let json_response = JSON.parse(response);
+    //console.log(json_response);
+    console.log(json_response['message']);
+    //.then(text => text.text());
+    //console.log(text['message']);
+
+    displayQuote(json_response['message']);
+}
+
+function displayQuote(x) {
+    //const quoteBox = document.querySelector('#js-quote-test');
+    //console.log('display quote working');
+    //const textMessage = document.createTextNode(x);
+
+    //quoteBox.appendChild(textMessage);
+
+    document.getElementById('js-quote-text').textContent = x;
+}
+
+getQuote();
